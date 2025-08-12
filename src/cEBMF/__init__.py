ca1aa85e06d918848b4b95b6743885a1cc69b8a0
WD <william.denault@gmail.com>
2025-08-12 10:16:17 +0200

    reorganising package

--- src/cEBMF/__init__.py
+++ src/cEBMF/__init__.py
@@ -1,9 +1,9 @@
-from .ash import fit_ash
-from .empirical_mdn import EmpiricalMDN
-from .ebnm_solver.ebnm_point_laplace import ebnm_point_laplace
-from .covariates.covariate_moderated_generalized_binary import CGB
-from .covariates.hard_covariate_moderated_generalized_binary import HCGB
-
+from .ebnm import fit_ash
+from .cebnm_solver import EmpiricalMDN
+from .ebnm import ebnm_point_laplace
+from .cebnm_solver import CGB
+from .cebnm_solver import HCGB
+ 
 __all__ = [
     "fit_ash", "EmpiricalMDN",
     "ebnm_point_laplace",
